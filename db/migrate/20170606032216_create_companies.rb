68be8f169e8198425b305320fc4c1caebfe7226c
cySheng <cyusheng93@gmail.com>
2017-06-06 12:10:31 +0800

    Regenerated users model, migration table controller and etc. Fixed routes for users too

--- db/migrate/20170606032216_create_companies.rb
+++ db/migrate/20170606032216_create_companies.rb
@@ -3,7 +3,6 @@ class CreateCompanies < ActiveRecord::Migration[5.1]
     create_table :companies do |t|
       t.string :title
       t.string :url
-      t.string :full_name
       t.string :logo
       t.text :description
       t.string :industry
@@ -11,15 +10,7 @@ class CreateCompanies < ActiveRecord::Migration[5.1]
       t.string :engineers
       t.string :core_stack, array: true
       t.string :stage
-
-      t.string :email, null: false
-      t.string :encrypted_password, limit: 128, null: false
-      t.string :confirmation_token, limit: 128
-      t.string :remember_token, limit: 128, null: false
-      t.timestamps null: false
+      t.references :user, foreign_key: true
     end
-
-    add_index :companies, :email
-    add_index :companies, :remember_token
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20170606032216) do
   create_table "companies", force: :cascade do |t|
     t.string "title"
     t.string "url"
-    t.string "full_name"
     t.string "logo"
     t.text "description"
     t.string "industry"
@@ -23,14 +22,20 @@ ActiveRecord::Schema.define(version: 20170606032216) do
     t.string "engineers"
     t.string "core_stack"
     t.string "stage"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_companies_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "full_name"
     t.string "email", null: false
     t.string "encrypted_password", limit: 128, null: false
     t.string "confirmation_token", limit: 128
     t.string "remember_token", limit: 128, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_companies_on_email"
-    t.index ["remember_token"], name: "index_companies_on_remember_token"
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
 end
