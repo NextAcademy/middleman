b0f294f31049cd582f1c0eacf30ebb7b4644633d
cySheng <cyusheng93@gmail.com>
2017-06-16 12:18:18 +0800

    Added ability to automatically redirect to versions page when the user logged in iss a superadmin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,4 +6,21 @@ class SessionsController < Clearance::SessionsController
     redirect_to root_path
   end
 
+   def create
+    @user = authenticate(params)
+
+    sign_in(@user) do |status|
+      if status.success?
+        if @user.superadmin?
+          redirect_to versions_path
+        else
+          redirect_back_or url_after_create
+        end
+      else
+        flash.now.notice = status.failure_message
+        render template: "sessions/new", status: :unauthorized
+      end
+    end
+  end
+
 end
